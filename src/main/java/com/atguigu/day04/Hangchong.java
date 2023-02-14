3049b001121614691c683f5b1307ed6942c5afcc
tsk <tsk18571467288@163.com>
2023-02-14 19:54:12 +0800

    tsk第一次提交

---
+++ src/main/java/com/atguigu/day04/Hangchong.java
@@ -0,0 +1,21 @@
+package com.atguigu.day04;
+
+/**
+ * @Author: TSK
+ * @Date：2023/2/14 19:53
+ */
+public class Hangchong {
+    private String name;
+
+    public Hangchong(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
